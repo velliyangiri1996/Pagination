9745984b9e9559005c47f7f4c95409cf26c83236
VELLIYANGIRI RAJAGAESH <57614171+velliyangiri1996@users.noreply.github.com>
2022-09-18 21:39:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,549 @@
+var tableData =[
+    {
+     "Name": "Bertrand.Miller",
+     "Place": "Tarynside",
+     "Phone": "381-223-0427 x2808",
+     "Company": "Price Group",
+     "Job": "Legacy Accountability Consultant",
+     "JobType": "Group",
+     "id": "1"
+    },
+    {
+     "Name": "Ford_Rau27",
+     "Place": "Muellerworth",
+     "Phone": "745-990-5638 x98536",
+     "Company": "McCullough Group",
+     "Job": "Central Division Facilitator",
+     "JobType": "Accountability",
+     "id": "2"
+    },
+    {
+     "Name": "Jean.Hilll92",
+     "Place": "New Amanda",
+     "Phone": "880.370.3045",
+     "Company": "Schaefer - Hills",
+     "Job": "Customer Intranet Consultant",
+     "JobType": "Quality",
+     "id": "3"
+    },
+    {
+     "Name": "Lavonne.Rutherford",
+     "Place": "Runolfssonstead",
+     "Phone": "(340) 618-3291",
+     "Company": "Cruickshank - Zemlak",
+     "Job": "Customer Group Technician",
+     "JobType": "Implementation",
+     "id": "4"
+    },
+    {
+     "Name": "Damon_VonRueden38",
+     "Place": "Lewisville",
+     "Phone": "(544) 552-3747",
+     "Company": "Thompson, Zieme and Skiles",
+     "Job": "Future Interactions Assistant",
+     "JobType": "Mobility",
+     "id": "5"
+    },
+    {
+     "Name": "Janiya.Streich41",
+     "Place": "Port Zoeystad",
+     "Phone": "308-240-3658",
+     "Company": "Spinka, Fritsch and Russel",
+     "Job": "Chief Security Director",
+     "JobType": "Solutions",
+     "id": "6"
+    },
+    {
+     "Name": "Alek_Lemke60",
+     "Place": "New Elitown",
+     "Phone": "1-980-949-3725 x78664",
+     "Company": "Wehner - Kovacek",
+     "Job": "Product Directives Specialist",
+     "JobType": "Configuration",
+     "id": "7"
+    },
+    {
+     "Name": "Celestino31",
+     "Place": "Connland",
+     "Phone": "279.977.8139",
+     "Company": "Hahn, Morar and Miller",
+     "Job": "Chief Research Coordinator",
+     "JobType": "Interactions",
+     "id": "8"
+    },
+    {
+     "Name": "Adalberto.Toy87",
+     "Place": "North Kyrafort",
+     "Phone": "242.350.9613 x0159",
+     "Company": "Funk LLC",
+     "Job": "Product Communications Technician",
+     "JobType": "Creative",
+     "id": "9"
+    },
+    {
+     "Name": "Shannon_Lowe7",
+     "Place": "South Wymanhaven",
+     "Phone": "(828) 317-8150 x2944",
+     "Company": "Auer, Zulauf and Thiel",
+     "Job": "Corporate Research Consultant",
+     "JobType": "Security",
+     "id": "10"
+    },
+    {
+     "Name": "Gilbert58",
+     "Place": "North D'angelofurt",
+     "Phone": "(873) 754-6965 x77282",
+     "Company": "Runolfsdottir LLC",
+     "Job": "Internal Paradigm Technician",
+     "JobType": "Identity",
+     "id": "11"
+    },
+    {
+     "Name": "Lonzo.OConner",
+     "Place": "Weberport",
+     "Phone": "1-681-583-1255 x3123",
+     "Company": "Pagac, Bernhard and Hackett",
+     "Job": "International Program Executive",
+     "JobType": "Usability",
+     "id": "12"
+    },
+    {
+     "Name": "Larry69",
+     "Place": "Fort Antonette",
+     "Phone": "(692) 725-8398 x36238",
+     "Company": "McLaughlin - Bergstrom",
+     "Job": "Customer Usability Representative",
+     "JobType": "Functionality",
+     "id": "13"
+    },
+    {
+     "Name": "Elsa_Braun57",
+     "Place": "New Tyreehaven",
+     "Phone": "942-431-3343 x70474",
+     "Company": "Bayer Inc",
+     "Job": "Forward Security Producer",
+     "JobType": "Directives",
+     "id": "14"
+    },
+    {
+     "Name": "Ayla.Balistreri98",
+     "Place": "Cerritos",
+     "Phone": "(654) 318-3816",
+     "Company": "Grant, Rolfson and Abernathy",
+     "Job": "Human Operations Representative",
+     "JobType": "Accounts",
+     "id": "15"
+    },
+    {
+     "Name": "Ignacio_Ziemann",
+     "Place": "Salem",
+     "Phone": "1-294-258-2215 x1274",
+     "Company": "Lockman LLC",
+     "Job": "Senior Branding Producer",
+     "JobType": "Solutions",
+     "id": "16"
+    },
+    {
+     "Name": "Joel_Krajcik1",
+     "Place": "South Lorenzabury",
+     "Phone": "729.708.7863",
+     "Company": "Bergstrom - Hettinger",
+     "Job": "Dynamic Functionality Coordinator",
+     "JobType": "Infrastructure",
+     "id": "17"
+    },
+    {
+     "Name": "Celestino48",
+     "Place": "Fayetteville",
+     "Phone": "535.541.9144 x20831",
+     "Company": "Schroeder - Reinger",
+     "Job": "Dynamic Factors Supervisor",
+     "JobType": "Configuration",
+     "id": "18"
+    },
+    {
+     "Name": "Johann.Donnelly",
+     "Place": "West Ethel",
+     "Phone": "(884) 200-1257",
+     "Company": "Langosh, Stehr and Abshire",
+     "Job": "Corporate Data Designer",
+     "JobType": "Group",
+     "id": "19"
+    },
+    {
+     "Name": "Laverna.Ledner",
+     "Place": "Fort Eliseoton",
+     "Phone": "607-967-6859 x6136",
+     "Company": "Spencer - Herzog",
+     "Job": "Principal Branding Coordinator",
+     "JobType": "Marketing",
+     "id": "20"
+    },
+    {
+     "Name": "Warren.Jaskolski68",
+     "Place": "East Perrymouth",
+     "Phone": "516-448-1499",
+     "Company": "Powlowski, Breitenberg and Walker",
+     "Job": "Forward Identity Supervisor",
+     "JobType": "Interactions",
+     "id": "21"
+    },
+    {
+     "Name": "Alberto_Baumbach",
+     "Place": "New Isobel",
+     "Phone": "754-460-7994",
+     "Company": "Kuhic and Sons",
+     "Job": "Senior Research Analyst",
+     "JobType": "Infrastructure",
+     "id": "22"
+    },
+    {
+     "Name": "Olga.Ward",
+     "Place": "Rockycester",
+     "Phone": "684-564-4054 x982",
+     "Company": "Ledner and Sons",
+     "Job": "Product Tactics Orchestrator",
+     "JobType": "Response",
+     "id": "23"
+    },
+    {
+     "Name": "Trudie.Toy98",
+     "Place": "Fort Ardellaburgh",
+     "Phone": "623.373.8907",
+     "Company": "Cummings, Farrell and Larkin",
+     "Job": "Regional Configuration Director",
+     "JobType": "Paradigm",
+     "id": "24"
+    },
+    {
+     "Name": "Mavis_Crist",
+     "Place": "South Monroe",
+     "Phone": "534-253-0618 x8898",
+     "Company": "Shields, Kohler and Lockman",
+     "Job": "Chief Configuration Agent",
+     "JobType": "Configuration",
+     "id": "25"
+    },
+    {
+     "Name": "Isabelle80",
+     "Place": "Lebsackside",
+     "Phone": "613.927.9446 x400",
+     "Company": "McDermott, Krajcik and Koepp",
+     "Job": "National Communications Architect",
+     "JobType": "Accountability",
+     "id": "26"
+    },
+    {
+     "Name": "Dexter3",
+     "Place": "Brandtland",
+     "Phone": "(996) 711-5591 x109",
+     "Company": "Klocko - Dickinson",
+     "Job": "Human Response Designer",
+     "JobType": "Data",
+     "id": "27"
+    },
+    {
+     "Name": "Ariane_Champlin",
+     "Place": "East Daphney",
+     "Phone": "542.324.6712 x6196",
+     "Company": "Marks Inc",
+     "Job": "Investor Creative Engineer",
+     "JobType": "Interactions",
+     "id": "28"
+    },
+    {
+     "Name": "Emmanuel93",
+     "Place": "Alexbury",
+     "Phone": "919.681.6719 x003",
+     "Company": "Conroy, Schultz and Collins",
+     "Job": "Senior Branding Analyst",
+     "JobType": "Markets",
+     "id": "29"
+    },
+    {
+     "Name": "Maida63",
+     "Place": "New Domenicafurt",
+     "Phone": "952-366-7001",
+     "Company": "Koelpin Inc",
+     "Job": "Direct Optimization Officer",
+     "JobType": "Markets",
+     "id": "30"
+    },
+    {
+     "Name": "Pat_Parisian",
+     "Place": "Yundtfield",
+     "Phone": "644.275.6437 x69156",
+     "Company": "Kessler - Jast",
+     "Job": "Product Configuration Orchestrator",
+     "JobType": "Configuration",
+     "id": "31"
+    },
+    {
+     "Name": "Kris.Lindgren96",
+     "Place": "Veldaburgh",
+     "Phone": "1-632-892-9708",
+     "Company": "Homenick Group",
+     "Job": "Future Applications Executive",
+     "JobType": "Interactions",
+     "id": "32"
+    },
+    {
+     "Name": "Selmer_Grant32",
+     "Place": "Giuseppeboro",
+     "Phone": "1-239-921-7106 x6939",
+     "Company": "Cruickshank - Zemlak",
+     "Job": "District Tactics Director",
+     "JobType": "Interactions",
+     "id": "33"
+    },
+    {
+     "Name": "Valerie_Aufderhar39",
+     "Place": "East Moniquefort",
+     "Phone": "1-339-840-3312",
+     "Company": "Zieme, Wilderman and Heathcote",
+     "Job": "Legacy Intranet Representative",
+     "JobType": "Response",
+     "id": "34"
+    },
+    {
+     "Name": "Jacinto34",
+     "Place": "Walnut Creek",
+     "Phone": "786-749-9103 x9274",
+     "Company": "Treutel, Lang and Davis",
+     "Job": "National Factors Associate",
+     "JobType": "Accounts",
+     "id": "35"
+    },
+    {
+     "Name": "Monique_Hoppe",
+     "Place": "Evansville",
+     "Phone": "255.214.1682",
+     "Company": "Haley Inc",
+     "Job": "Regional Accounts Specialist",
+     "JobType": "Security",
+     "id": "36"
+    },
+    {
+     "Name": "Amely.Rempel15",
+     "Place": "Milfordworth",
+     "Phone": "(834) 679-6298 x33452",
+     "Company": "Schoen, Boehm and Brekke",
+     "Job": "Customer Paradigm Strategist",
+     "JobType": "Paradigm",
+     "id": "37"
+    },
+    {
+     "Name": "Presley_Daugherty47",
+     "Place": "McDermottstad",
+     "Phone": "323-862-1850",
+     "Company": "Crooks LLC",
+     "Job": "Corporate Marketing Orchestrator",
+     "JobType": "Accounts",
+     "id": "38"
+    },
+    {
+     "Name": "Kelton.Corkery9",
+     "Place": "Delphiaburgh",
+     "Phone": "261.251.8572 x4381",
+     "Company": "Rodriguez - Ziemann",
+     "Job": "National Tactics Technician",
+     "JobType": "Factors",
+     "id": "39"
+    },
+    {
+     "Name": "Imelda40",
+     "Place": "Alafaya",
+     "Phone": "1-793-778-5488",
+     "Company": "Tremblay, Larkin and Brown",
+     "Job": "Chief Infrastructure Orchestrator",
+     "JobType": "Applications",
+     "id": "40"
+    },
+    {
+     "Name": "Pierre45",
+     "Place": "Wilkinsonstead",
+     "Phone": "(515) 999-3995 x15056",
+     "Company": "Kutch, Crona and Blick",
+     "Job": "Customer Web Supervisor",
+     "JobType": "Response",
+     "id": "41"
+    },
+    {
+     "Name": "Buford14",
+     "Place": "New Berta",
+     "Phone": "367.634.9352",
+     "Company": "Lubowitz and Sons",
+     "Job": "Regional Creative Administrator",
+     "JobType": "Assurance",
+     "id": "42"
+    },
+    {
+     "Name": "Donnell_Sawayn",
+     "Place": "Dietrichboro",
+     "Phone": "(480) 943-9205",
+     "Company": "Hermann LLC",
+     "Job": "Regional Usability Manager",
+     "JobType": "Data",
+     "id": "43"
+    },
+    {
+     "Name": "Jaycee_Crona3",
+     "Place": "Indianapolis",
+     "Phone": "1-952-398-6298 x95744",
+     "Company": "Lesch LLC",
+     "Job": "Senior Quality Associate",
+     "JobType": "Research",
+     "id": "44"
+    },
+    {
+     "Name": "Amalia79",
+     "Place": "Beaumont",
+     "Phone": "357-297-3979",
+     "Company": "Bartell, Vandervort and Rau",
+     "Job": "Principal Paradigm Producer",
+     "JobType": "Creative",
+     "id": "45"
+    },
+    {
+     "Name": "Cleve.Cassin73",
+     "Place": "Boca Raton",
+     "Phone": "(428) 826-7221",
+     "Company": "McLaughlin, Huel and Grady",
+     "Job": "Regional Integration Consultant",
+     "JobType": "Program",
+     "id": "46"
+    },
+    {
+     "Name": "Verdie.Hirthe66",
+     "Place": "Robelstead",
+     "Phone": "1-639-550-4411",
+     "Company": "Pfannerstill - Feil",
+     "Job": "Principal Web Agent",
+     "JobType": "Functionality",
+     "id": "47"
+    },
+    {
+     "Name": "Aileen_Nikolaus23",
+     "Place": "New Lavada",
+     "Phone": "1-416-871-6525 x528",
+     "Company": "Willms LLC",
+     "Job": "Future Functionality Facilitator",
+     "JobType": "Brand",
+     "id": "48"
+    },
+    {
+     "Name": "Maya50",
+     "Place": "Rhodaburgh",
+     "Phone": "325.206.8327",
+     "Company": "Hammes - Jones",
+     "Job": "Internal Optimization Producer",
+     "JobType": "Interactions",
+     "id": "49"
+    },
+    {
+     "Name": "Ephraim38",
+     "Place": "New Camroncester",
+     "Phone": "1-475-362-1845",
+     "Company": "Carter - Ward",
+     "Job": "Corporate Security Planner",
+     "JobType": "Metrics",
+     "id": "50"
+    }
+   ]
+// above data link = "https://631d7002789612cd07aa9cc4.mockapi.io/Page"
+
+console.log(typeof tableData)
+var state = {
+'querySet': tableData,
+'page': 1,
+'rows': 5,
+'window': 5,
+}
+
+buildTable()
+
+function pagination(querySet, page, rows) {
+
+var trimStart = (page - 1) * rows
+var trimEnd = trimStart + rows
+
+var trimmedData = querySet.slice(trimStart, trimEnd)
+
+var pages = Math.round(querySet.length / rows);
+
+return {
+    'querySet': trimmedData,
+    'pages': pages,
+}
+}
+
+function pageButtons(pages) {
+var wrapper = document.getElementById('pagination-wrapper')
+
+wrapper.innerHTML = ``
+console.log('Pages:', pages)
+
+var maxLeft = (state.page - Math.floor(state.window / 2))
+var maxRight = (state.page + Math.floor(state.window / 2))
+
+if (maxLeft < 1) {
+    maxLeft = 1
+    maxRight = state.window
+}
+
+if (maxRight > pages) {
+    maxLeft = pages - (state.window - 1)
+    
+    if (maxLeft < 1){
+        maxLeft = 1
+    }
+    maxRight = pages
+}
+
+
+
+for (var page = maxLeft; page <= maxRight; page++) {
+    wrapper.innerHTML += `<button value=${page} class="page btn btn-sm btn-info">${page}</button>`
+}
+
+if (state.page != 1) {
+    wrapper.innerHTML = `<button value=${1} class="page btn btn-sm btn-info">&#171; First</button>` + wrapper.innerHTML
+}
+
+if (state.page != pages) {
+    wrapper.innerHTML += `<button value=${pages} class="page btn btn-sm btn-info">Last &#187;</button>`
+}
+
+$('.page').on('click', function() {
+    $('#table-body').empty()
+
+    state.page = Number($(this).val())
+
+    buildTable()
+})
+
+}
+
+function buildTable() {
+var table = $('#table-body')
+
+var data = pagination(state.querySet, state.page, state.rows)
+var myList = data.querySet;
+var i = 1;
+for ( i  in myList) {
+    //Keep in mind we are using "Template Litterals to create rows"
+    var row = `<tr>
+              <td>${myList[i].id}</td>
+              <td>${myList[i].Name}</td>
+              <td>${myList[i].Place}</td>
+              <td>${myList[i].Company}</td>
+              <td>${myList[i].Job}</td>
+              <td>${myList[i].JobType}</td>
+              <td>${myList[i].Phone}</td>
+              </tr>`
+    
+    table.append(row)
+}
+
+pageButtons(data.pages)
+}
